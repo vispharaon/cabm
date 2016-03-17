974f078c03641b4973143ddc820e6dbfdeddb83c
Amar Hajro <amarhajro@gmail.com>
2016-03-17 21:46:14 +0100

    developer side - CAB info page first part done. developer side - developer tasks table imported.

--- js/controllers/CABHomeDeveloperController.js
+++ js/controllers/CABHomeDeveloperController.js
@@ -1,10 +1,26 @@
-use 'strict'
+'use strict';
 
-angular
+    angular
         .module('app')
         .controller('CABHomeDeveloperController', CABHomeDeveloperController);
 
-    CABHomeDeveloperController.$inject = ['$location'];
-    function CABHomeDeveloperController($location) {
+    CABHomeDeveloperController.$inject = ['$location', '$scope', 'CabData'];
+    function CABHomeDeveloperController($location, $scope, CabData) {
         var vmd = this;
+		
+		CabData.getAllCABAPI(function(cabs){
+          $scope.allCabs = cabs;
+        });
+
+        $scope.filterCABShow = 13;
+
+        vmd.selected = [];
+        $scope.openCharacterDetails = function () {
+            vmd.selected = this.session;
+            console.log('cabDetails.html/id=' + vmd.selected);
+           // CabData.getDataForCABTest(); 
+            window.location = '#/cabDetails';
+            console.log(vmd.selected);
+        }
+
     }
--- js/controllers/DevelopmentController.js
+++ js/controllers/DevelopmentController.js
@@ -1,4 +1,4 @@
-use 'strict'
+'use strict';
 
 angular
         .module('app')
@@ -7,4 +7,60 @@ angular
     DevelopmentController.$inject = ['$location'];
     function DevelopmentController($location) {
         var vmd = this;
+
+        $(function() {
+    $( "#datepicker" ).datepicker();
+	$("#iShow").hide();
+	$("#infoNewDeveloper").hide();
+	$("#aBackToTaskTable").hide();
+	$("#divTaskInfo").hide();
+	
+  });
+  
+$(document).ready(function(){
+    $("#iHide").click(function(){
+        $("#formShowHide").hide(500);
+		$("#iShow").show();
+		$("#iHide").hide();
+		$("#infoNewDeveloper").show();
+		$("#infoNewDeveloperEmpty").hide();
+    });
+	
+	 $("#iShow").click(function(){
+        $("#formShowHide").show(500);
+		$("#iShow").hide();
+		$("#iHide").show();
+		$("#infoNewDeveloper").hide();
+		$("#infoNewDeveloperEmpty").show();
+    });
+	
+	$('#jqTable').DataTable();
+	
+	$("#tbodyTasksTableItem").click(function(){
+        $("#aBackToTaskTable").show();
+		$("#divTaskInfo").show(500);
+		$("#divTasksTable").hide();
+    });
+	$("#aBackToTaskTable").click(function(){
+        $("#aBackToTaskTable").hide();
+		$("#divTaskInfo").hide();
+		$("#divTasksTable").show(800);
+    });
+	
+	$("#progressBarForTaskAll").hover(function(){
+        $("#progressBarForTask").hide();
+		$("#hiddenProgressBar").show();
+    },
+	function() {
+		$("#progressBarForTask").show();
+		$("#hiddenProgressBar").hide();
+	});
+	
+	/*$("#hiddenProgressBar").hover(function(){
+        $( this ).hide();
+		$("#progressBarForTask").show();
+    });*/
+});
+
+
     }
